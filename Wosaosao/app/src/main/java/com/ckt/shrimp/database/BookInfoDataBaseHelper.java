89ab2a97decb4a94fb213c88749dc54a289205dc
shrimpcolo <tanhq-2005@163.com>
2015-06-10 14:55:58 +0800

    [changed] Add some new strings, and delete the chinese annotation.
    modified:   Wosaosao/app/src/main/java/com/ckt/shrimp/database/BookInfoD
    ataBaseHelper.java
    modified:   Wosaosao/app/src/main/java/com/ckt/shrimp/wosaosao/ScanningA
    ctivity.java
    modified:   Wosaosao/app/src/main/res/values-zh-rCN/strings.xml
    modified:   Wosaosao/app/src/main/res/values/strings.xml

--- Wosaosao/app/src/main/java/com/ckt/shrimp/database/BookInfoDataBaseHelper.java
+++ Wosaosao/app/src/main/java/com/ckt/shrimp/database/BookInfoDataBaseHelper.java
@@ -3,7 +3,6 @@ package com.ckt.shrimp.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /**
  * Created by ckt on 06/03/15.
